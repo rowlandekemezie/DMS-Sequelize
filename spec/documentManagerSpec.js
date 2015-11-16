3b9f8aaae78cd2eb9e15dfc2c93d2c3d57ce2571
rowland <rowlandekemezie@gmail.com>
2015-11-16 02:23:41 +0100

    test(test): add spec file
    -add documentManager.js
    -install jasmine-node

---
+++ spec/documentManagerSpec.js
@@ -0,0 +1,17 @@
+describe("User", function(){
+  it('should return a role', function(){
+    expect(1).toBe(1);
+  });
+});
+
+describe("Role", function(){
+
+});
+
+describe("Document", function(){
+
+});
+
+describe("Search", function(){
+
+});
