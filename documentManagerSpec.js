dbea8009cbcc52306cdd7e8e8a8a41c0dfb64244
rowland <rowlandekemezie@gmail.com>
2015-11-26 04:49:52 +0100

    test(core): add a test function
    -seed the database for test

--- documentManagerSpec.js
+++ documentManagerSpec.js
@@ -1,42 +1,45 @@
 var dms = require('./documentManager'),
   schema = require('./schema');
 
+var expectedData = {
+  firstName: 'michael',
+  lastName: 'Ekwe',
+  Role: 'Admin'
+};
+
+console.log(dms);
 describe("User", function() {
   beforeEach(function(done) {
-    schema.User.destroy({
-      where:{}
-    }).then(function() {
-      dms.createUser('michael', 'Ekwe', 'Admin', function() {
-        done();
-      });
-    });
-  });
-
-  afterEach(function(done) {
-    schema.User.destroy({
-      where: {}
-    }).then(function() {
-      schema.Role.destroy({
-        where: {}
-      });
+   dms.createUser('michael', 'row@gmail.com', 'Ekwe', 'Abu', 'row111', function(err) {
+      if(err) throw err;
       done();
     });
   });
 
-  it('should return that a new user is unique', function() {
-    schema.User.findOne({
+  // afterEach(function(done) {
+  //   schema.User.destroy({
+  //     where: {}
+  //   }).then(function() {
+  //     schema.Role.destroy({
+  //       where: {}
+  //     });
+  //     done();
+  //   });
+  // }, 20000);
+  it('should return that a new user is unique', function(done) {
+    schema.User.findAll({
       where: {
-        firstName: "michaele"
+        userName: 'michael'
       }
-    }).then(function(user) {
+    }).then(function(user){
       expect(user).toBeDefined();
-      expect(user.firstName).toBeUndefined();
-      expect(user.length).toBe(1);
-      expect(user.length).not.toBeUndefined();
+      expect(user).toEqual("michael");
+      // expect(user.length).toBe(2);
+      // expect(user.length).toBeUndefined();
       done();
     });
-  });
-});
+ });
+}, 20000);
 
 // describe("Role", function(){
 //
